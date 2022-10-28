712db028c19158fe30a6a7955241081edcf31778
nhussaini <nasrullah_hussaini@yahoo.com>
2022-10-28 19:01:44 -0400

    add BookResource

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,16 +1,35 @@
 package org.agoncal.quarkus.microservices.book;
 
-import javax.ws.rs.GET;
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.Instant;
 
 @Path("/api/books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Inject
+    Logger logger;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createABook(@FormParam("title") String title, @FormParam("author") String author, @FormParam("year") int yearOfPublication, @FormParam("genre") String genre) {
+        Book book = new Book();
+        book.isbn13 = "will come later";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        logger.info("Book created: " + book);
+        return Response.status(201).entity(book).build();
     }
 }
