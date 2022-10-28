226acb4f34bd6777773fa774cc1f221ebca337ec
nhussaini <nasrullah_hussaini@yahoo.com>
2022-10-28 19:43:02 -0400

    document the book microservice

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,5 +1,7 @@
 package org.agoncal.quarkus.microservices.book;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
@@ -13,6 +15,7 @@ import javax.ws.rs.core.Response;
 import java.time.Instant;
 
 @Path("/api/books")
+@Tag(name = "Book REST endpoint")
 public class BookResource {
 
     @Inject
@@ -21,6 +24,10 @@ public class BookResource {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Operation(
+            summary = "Creates a Book",
+            description = "Creates a Book with an ISBN number"
+    )
     public Response createABook(@FormParam("title") String title, @FormParam("author") String author, @FormParam("year") int yearOfPublication, @FormParam("genre") String genre) {
         Book book = new Book();
         book.isbn13 = "will come later";
